302f21eab01dde5d92c78b4a0f6a40c65f65fbf0
jellyfish700 <jellyfish.apple200@gmail.com>
2024-10-19 20:55:15 +0900

    エンドポイントを追加

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import time
+import uuid
 
 app = FastAPI()
 
@@ -11,6 +14,47 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
+class Room(BaseModel):
+    roomName: str
+
 @app.get("/")
 async def root():
     return {"msg": "Hello World!!!!"}
+
+#新規ルーム作成
+@app.post("/rooms")
+async def root(room_request:Room):
+    return {
+      "roomId": uuid.uuid4(),
+      "roomName": room_request.roomName
+    }
+
+#ルーム情報取得
+@app.get("/rooms/{room_id}")
+async def root(room_id:str):
+    return{
+    "id": room_id,
+    "name": "Sample Room Name",
+    "messages": [
+        {
+            "id": "eee9a636-ea03-408e-86b6-5b75813a7d19",
+            "message": "Hello, World!",
+            "createdAt": 1729249549
+        },
+        {
+            "id": "eee9a636-ea03-408e-86b6-5b75813a7d19",
+            "message": "Goodbye, World!",
+            "createdAt": 1729249549
+        }
+    ]
+}
+
+#メッセージ新規作成
+@app.post("/rooms/{room_id}/messages")
+async def root(room_id:str):
+    return {
+      "id": "33d9c826-f54d-4b94-8551-005e276c1108",
+      "roomId": room_id,
+      "message": "Hello, World!",
+      "createdAt": int(time.time())
+    }
