457192979876458e28cb0c3439c4cb316d56c3b8
zacker <iszkmasaki@gmail.com>
2024-10-19 21:59:29 +0900

    feat(view): デフォルトのスタイルを適用

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,43 @@
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+    return <>
+        <style jsx global>{`
+            html,
+            body,
+            #__next {
+                height: 100%;
+                margin: 0;
+                padding: 0;
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+                margin: 0;
+            }
+
+            a {
+                text-decoration: none;
+                color: inherit;
+                appearance: none;
+            }
+
+            a:hover {
+                cursor: pointer;
+                text-decoration: none !important;
+                appearance: none;
+            }
+
+            ol,
+            ul {
+                list-style: none;
+            }
+        `}
+        </style>
+        <Component {...pageProps} />
+    </>
 }
