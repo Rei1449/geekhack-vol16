e99e090761564740bafc6f285a72aecd8f33b8e0
zacker <iszkmasaki@gmail.com>
2024-10-20 21:36:47 +0900

    feat(data): APIクライアントを作成

---
+++ frontend/src/data/RoomApi.ts
@@ -0,0 +1,44 @@
+class RoomApi {
+    async createRoom({
+        roomName
+    }: CreateRoomRequest): Promise<Room> {
+        throw new Error('implement me!');
+    }
+
+    async getRoom({
+        roomId
+    }: GetRoomsRequest): Promise<Room | null> {
+        throw new Error('implement me!');
+    }
+
+    async sendMessage({
+        id,
+        message,
+        roomId,
+    }: SendMessageRequest): Promise<Message> {
+        throw new Error('implement me!');
+    }
+
+    observeRoom({ }: ObserveRoom): void {
+        throw new Error('implement me!');
+    }
+}
+
+type CreateRoomRequest = {
+    roomName: string
+}
+
+type GetRoomsRequest = {
+    roomId: string
+}
+
+type SendMessageRequest = {
+    id: string;
+    message: string;
+    roomId: string;
+}
+
+type ObserveRoom = {
+    roomId: string
+    onMessage: ({ message }: { message: Message }) => void
+}
