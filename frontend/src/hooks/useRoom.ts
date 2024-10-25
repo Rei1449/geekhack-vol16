e56c3fcdcbd2c6faee8255f842aef96fdb37c519
zacker <iszkmasaki@gmail.com>
2024-10-25 20:32:52 +0900

    feat(hooks): apiを介して新規作成したメッセージが送信されるようにする

--- frontend/src/hooks/useRoom.ts
+++ frontend/src/hooks/useRoom.ts
@@ -2,15 +2,16 @@ import { useEffect, useState } from 'react';
 import { RoomApi } from 'src/data/RoomApi';
 import { Message } from 'src/models/Message';
 import { Room } from 'src/models/Room';
+import { v4 as uuidv4 } from 'uuid';
 
 export const useRoom = ({ roomId }: { roomId: string }) => {
+  const api = new RoomApi();
   const [room, setRoom] = useState<Room | null>(null);
 
-  const sendMessage = ({ message }: { message: string }) => {
-    console.log(message);
+  const sendMessage = async ({ message }: { message: string }) => {
+    const messageId = uuidv4();
+    await api.sendMessage({ id: messageId, message, roomId });
   };
-
-  const api = new RoomApi();
 
   useEffect(() => {
     api
