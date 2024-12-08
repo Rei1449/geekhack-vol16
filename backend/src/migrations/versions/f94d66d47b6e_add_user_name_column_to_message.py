ba18191f90f41a7745e84e37c8b587d773e5714a
jellyfish700 <jellyfish.apple200@gmail.com>
2024-12-08 15:10:11 +0900

    いらないコード消したよ

--- backend/src/migrations/versions/f94d66d47b6e_add_user_name_column_to_message.py
+++ backend/src/migrations/versions/f94d66d47b6e_add_user_name_column_to_message.py
@@ -8,15 +8,12 @@ Create Date: 2024-12-08 05:46:52.188489
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = 'f94d66d47b6e'
 down_revision: Union[str, None] = '509a99624551'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
-
 
 def upgrade() -> None:
     op.execute(
