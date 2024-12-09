b2823f4998156b5347ec0d1a1c67e0b742e1d277
zacker <iszkmasaki@gmail.com>
2024-12-09 17:56:47 +0900

    feat: clientをuseStateで管理

--- frontend/src/hooks/room/useScreenShare.ts
+++ frontend/src/hooks/room/useScreenShare.ts
@@ -4,7 +4,7 @@ import {
   ILocalVideoTrack,
   IRemoteVideoTrack,
 } from 'agora-rtc-sdk-ng';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useScreenShare = ({
   roomId,
@@ -13,7 +13,7 @@ export const useScreenShare = ({
   roomId: string;
   uid: string;
 }) => {
-  const clientRef = useRef<IAgoraRTCClient | null>(null);
+  const [client, setClient] = useState<IAgoraRTCClient | null>(null);
 
   const [isHost, setIsHost] = useState(false);
   const [screenTrack, setScreenTrack] = useState<
@@ -22,10 +22,10 @@ export const useScreenShare = ({
 
   const startScreenShare = useCallback(async () => {
     if (!roomId) return;
-    if (!clientRef.current) return;
+    if (!client) return;
 
     // 切断中は接続できない
-    const isDisconnecting = clientRef.current.connectionState === 'DISCONNECTING';
+    const isDisconnecting = client.connectionState === 'DISCONNECTING';
     if (isDisconnecting) {
       return;
     }
@@ -41,24 +41,24 @@ export const useScreenShare = ({
       })) as ILocalVideoTrack;
       setScreenTrack(screenTrack);
 
-      await clientRef.current.publish(screenTrack);
+      await client.publish(screenTrack);
     } catch (error) {
       console.error(error);
       setScreenTrack(null);
       setIsHost(false);
     }
-  }, [roomId, uid]);
+  }, [client, roomId, uid]);
 
   const stopScreenShare = useCallback(async () => {
     setIsHost(false);
 
-    if (!clientRef.current) return;
+    if (!client) return;
     if (!screenTrack) return;
 
     try {
       (screenTrack as ILocalVideoTrack).stop();
       (screenTrack as ILocalVideoTrack).close();
-      await clientRef.current.unpublish(screenTrack as ILocalVideoTrack);
+      await client.unpublish(screenTrack as ILocalVideoTrack);
     } catch (error) {
       console.error({
         tag: 'useScreenShare',
@@ -76,24 +76,22 @@ export const useScreenShare = ({
       user: IAgoraRTCRemoteUser,
       mediaType: 'audio' | 'video' | 'datachannel',
     ) => {
-      console.log({ tag: 'useScreenShare', action: 'user-published', isHost });
-     
+      console.log({ tag: 'useScreenShare', action: 'user-published', mediaType, user, isHost });
+
       // ホストの場合は何もしない
       if (isHost) return;
 
       // 画面共有のみを受信する
-      if(mediaType !== 'video') return;
+      if (mediaType !== 'video') return;
 
-      const client = clientRef.current;
       if (!client) return;
-      
       await client.subscribe(user, mediaType);
 
       const remoteScreenTrack = user.videoTrack;
       if (!remoteScreenTrack) return;
       setScreenTrack(remoteScreenTrack);
     },
-    [],
+    [client, isHost],
   );
 
   const handleUserUnpublished = useCallback(() => {
@@ -111,15 +109,19 @@ export const useScreenShare = ({
       uid: string | null;
       client: IAgoraRTCClient;
     }) => {
+      console.log({ tag: 'useScreenShare', action: 'joinRoom', roomId, uid });
       if (!roomId || !uid) return;
 
       try {
         const isJoined = client.connectionState === 'CONNECTED';
         if (isJoined) {
-          console.log({ tag: 'useScreenShare', action: 'already-joined-and-leave' });
+          console.log({
+            tag: 'useScreenShare',
+            action: 'already-joined-and-leave',
+          });
           await client.leave();
         }
-        
+
         console.log({ tag: 'useScreenShare', action: 'joinRoom', roomId, uid });
         await client.join(process.env.AGORA_APP_ID!, roomId, null, uid);
 
@@ -141,26 +143,32 @@ export const useScreenShare = ({
     (async () => {
       const AgoraRTC = (await import('agora-rtc-sdk-ng')).default;
       const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-      clientRef.current = client;
+      setClient(client);
     })();
 
     return () => {
-      if (!clientRef.current) return;
+      if (!client) return;
       console.log({ tag: 'useScreenShare', action: 'cleanup' });
-      clientRef.current.leave();
+      client.leave();
     };
   }, []);
 
   useEffect(() => {
-    if (!clientRef.current) return;
-    joinRoom({ client: clientRef.current!, roomId, uid });
+    if (!client) return;
+
+    // 入室直後は接続がうまくいかないことがある
+    const id = setTimeout(() => {
+      joinRoom({ client, roomId, uid });
+    }, 1000);
 
     return () => {
-      if (!clientRef.current) return;
-      clientRef.current.off('user-published', handleUserPublished);
-      clientRef.current.off('user-unpublished', handleUserUnpublished);
-    }
-  }, [uid, roomId]);
+      clearTimeout(id);
+
+      if (!client) return;
+      client.off('user-published', handleUserPublished);
+      client.off('user-unpublished', handleUserUnpublished);
+    };
+  }, [client !== null, uid, roomId]);
 
   return {
     isSharing: isHost,
