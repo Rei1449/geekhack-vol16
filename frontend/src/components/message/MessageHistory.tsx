46f2a59f502f60217025c3e45148059085286c44
jellyfish700 <jellyfish.apple200@gmail.com>
2024-12-11 19:57:26 +0900

    scoreが1のメッセージのみを表示

--- frontend/src/components/message/MessageHistory.tsx
+++ frontend/src/components/message/MessageHistory.tsx
@@ -50,7 +50,7 @@ export function MessageHistory({
             }}
           >
             {messages
-              .filter((messages) => !defaultMessage.includes(messages.message))
+              .filter((messages) => !defaultMessage.includes(messages.message) && messages.score == 1)
               .map((message) => (
                 <VStack style={{ gap: 0 }} key={message.id}>
                   <MessageText
